0e18e771cf9688ea471f4bbf8ac269951412fbab
Alan Luo <yaulaannl@yahoo.com>
2016-10-12 02:59:30 +0000

    Fix bug in header.php

--- header.php
+++ header.php
@@ -37,7 +37,6 @@
 </head>
 
 <body <?php body_class(); ?>>
-<div id="page" class="hfeed site">
 	<a class="skip-link screen-reader-text" href="#content"><?php esc_html_e( 'Skip to content', 'multitopics' ); ?></a>
 
 	<!-- HEADER
