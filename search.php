364aa2e93de8ee428c4c83f6c70ec40568381f0c
Alan Luo <yaulaannl@yahoo.com>
2016-11-09 08:14:13 +0000

    fixed search.php code styling.

--- search.php
+++ search.php
@@ -33,12 +33,7 @@ if(get_locale() == 'zh_TW'){
 		<?php if ( have_posts() ) : ?>  
 		  <?php while ( have_posts() ) : the_post(); ?>
 		    <?php if( $post->post_type=='page' ) continue; /*ignore page */ ?>	
-                    <?php
-                    	/*
-                         * Include the Post-Format-specific template for the content.                                                                                                 * If you want to override this in a child theme, then include a file                                                                                         * called content-___.php (where ___ is the Post Format name) and that will be used instead.
-                        */
-                        get_template_part( 'template-parts/content', get_post_format() );
-		    ?>
+                    <?php get_template_part( 'template-parts/content', get_post_format() ); ?>
 		  <?php endwhile; ?>
                   <?php the_posts_navigation(); ?>
 		<?php else : ?>                                                                                                           <?php get_template_part( 'template-parts/content', 'none' ); ?>
